aeb15da2083a6db2d46ff5996bf0ff6fc5c9774e
Sanjay V S <sanjayvs2004@gmail.com>
2023-09-21 13:19:46 +0530

    web stream

--- web.py
+++ web.py
@@ -1,3 +1,4 @@
+from imutils.video import VideoStream
 from flask import Response
 from flask import Flask
 from flask import render_template
@@ -7,17 +8,34 @@ import datetime
 import imutils
 import time
 import cv2
-import numpy as np
+import cvzone
+from ultralytics import YOLO
+import math
+import torch
+
+torch.cuda.set_device(0)
 
 outputFrame = None
 lock = threading.Lock()
+
 app = Flask(__name__)
 
-vs = cv2.VideoCapture(0)
+#vs = VideoStream(usePiCamera=1).start()
+vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+model = YOLO("yolov8n.pt")
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
 
 @app.route("/")
 def index():
@@ -25,19 +43,24 @@ def index():
 
 def detect_motion(frameCount):
 	global vs, outputFrame, lock
-	total = 0
 	while True:
-		ret, frame = vs.read()
-		frame = cv2.resize(frame, (640, 480))
-		gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-		boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
-		boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
-		for (xA, yA, xB, yB) in boxes:
-		    cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
-		    cv2.putText(frame, "Human!", (xA, yA), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,0), 2, cv2.LINE_AA)
-		(flag, encodedImage) = cv2.imencode(".jpg", frame)
+		img = vs.read()
+		results = model(img, stream=True)
+		for r in results:
+			boxes = r.boxes
+			for box in boxes:
+				x1, y1, x2,  y2 = box.xyxy[0]
+				x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+				w, h = (x2-x1), (y2-y1)
+				print(x1, y1, x2, y2)
+				cvzone.cornerRect(img, (x1, y1, w, h))
+				conf = math.ceil((box.conf[0]*100))
+				print(conf)
+				cls = int(box.cls[0])
+				cvzone.putTextRect(img, f'{classNames[cls]} {conf}%', (max(0, x1), max(0, y1 + 35)), scale=1, thickness=1)	
+		
 		with lock:
-			outputFrame = frame.copy()
+			outputFrame = img.copy()
 
 def generate():
 	global outputFrame, lock
@@ -48,24 +71,22 @@ def generate():
 			(flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
 			if not flag:
 				continue
-		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+			bytearray(encodedImage) + b'\r\n')
 
 @app.route("/video_feed")
 def video_feed():
-	return Response(generate(), mimetype = "multipart/x-mixed-replace; boundary=frame")
-
-
-app = Flask(__name__)
-
-@app.route("/")
-def video_feed():
-	return Response(generate(), mimetype = "multipart/x-mixed-replace; boundary=frame")
+	return Response(generate(),
+		mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 if __name__ == '__main__':
 	ap = argparse.ArgumentParser()
-	ap.add_argument("-i", "--ip", type=str, required=True,help="ip address of the device")
-	ap.add_argument("-o", "--port", type=int, required=True,help="ephemeral port number of the server (1024 to 65535)")
-	ap.add_argument("-f", "--frame-count", type=int, default=32,help="# of frames used to construct the background model")
+	ap.add_argument("-i", "--ip", type=str, required=True,
+		help="ip address of the device")
+	ap.add_argument("-o", "--port", type=int, required=True,
+		help="ephemeral port number of the server (1024 to 65535)")
+	ap.add_argument("-f", "--frame-count", type=int, default=32,
+		help="# of frames used to construct the background model")
 	args = vars(ap.parse_args())
 	t = threading.Thread(target=detect_motion, args=(
 		args["frame_count"],))
